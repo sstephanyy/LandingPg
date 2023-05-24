7ba87bfc861b7f750b64dd73dace7a904f4231fa
sstephanyy <128330097+sstephanyy@users.noreply.github.com>
2023-05-24 14:56:51 -0300

    1 file modified

--- src/components/Landing/commom/Area.jsx
+++ src/components/Landing/commom/Area.jsx
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 const OutsideNavbar = styled.div`
   display: flex;
   justify-content: center;
-  width: 100%;
   background-color: #EF4444;
-`
+  width: 100%;
+  padding: 0px 210px;`
+
 const InsideNav = styled.div`
   padding-left: 7px;
   padding-right: 7px;
   width: 100%;
+  background-color: green;
 
   @media (min-width: 1280px) {
     width: 1200px;
@@ -19,8 +21,9 @@ const InsideNav = styled.div`
 export default function Area (props) {
   return (
     <>
-    <OutsideNavbar>{props.outsideNavbar}</OutsideNavbar>
-    <InsideNav>{props.insideNav}</InsideNav>
+      <OutsideNavbar>
+        <InsideNav>{props.children}</InsideNav>
+      </OutsideNavbar>
     </>
   );
 }
