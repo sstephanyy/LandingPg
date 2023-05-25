4876aba8e503d5464214d622e31c78e53b0e9ee5
sstephanyy <128330097+sstephanyy@users.noreply.github.com>
2023-05-25 15:21:36 -0300

    2 files modified

--- src/components/Landing/header/NavRoots.jsx
+++ src/components/Landing/header/NavRoots.jsx
@@ -24,11 +24,11 @@ export const NavRoots = () => {
               width="17"
               height="17"
               viewBox="0 0 24 24"
-              stroke-width="2"
+              strokeWidth="2"
               stroke="currentColor"
               fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
               <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8"></path>
