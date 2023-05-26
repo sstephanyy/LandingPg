c87d7071f5b2dba4741c051056639bfede09ff0d
sstephanyy <128330097+sstephanyy@users.noreply.github.com>
2023-05-26 11:03:34 -0300

    2 files modified

--- src/components/Landing/trending/MainContent.jsx
+++ src/components/Landing/trending/MainContent.jsx
@@ -13,13 +13,32 @@ export default function MainContent() {
           </div>
         </div>
  
-        <div className="descrip-text">
-          <p>Financial platform that makes your life easier!!</p>
+        <div >
+          <p className="paragh-text">Financial platform that makes your life easier!!</p>
         </div>
 
         <div className="buttons">
-          <button className="button-start">Start Now</button>
-          <button className="button-video">Watch the Video</button>
+          <button className="button-start">
+            Start Now
+
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M5 12l14 0"></path>
+            <path d="M15 16l4 -4"></path>
+            <path d="M15 8l4 4"></path>
+            </svg>
+
+          </button>
+          
+
+          <button className="button-video">
+            Watch the Video
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-zoom" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M17.011 9.385v5.128l3.989 3.487v-12z"></path>
+            <path d="M3.887 6h10.08c1.468 0 3.033 1.203 3.033 2.803v8.196a.991 .991 0 0 1 -.975 1h-10.373c-1.667 0 -2.652 -1.5 -2.652 -3l.01 -8a.882 .882 0 0 1 .208 -.71a.841 .841 0 0 1 .67 -.287z"></path>
+          </svg>
+          </button>
         </div>
       </div>
     </div>
