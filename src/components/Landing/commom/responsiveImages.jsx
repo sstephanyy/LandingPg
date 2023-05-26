ae850f108baed535c4c7ab68a1047fe252a7d3b5
sstephanyy <128330097+sstephanyy@users.noreply.github.com>
2023-05-26 15:45:20 -0300

    2 files modified

--- src/components/Landing/commom/responsiveImages.jsx
+++ src/components/Landing/commom/responsiveImages.jsx
@@ -1,27 +1,3 @@
-import styled from 'styled-components';
-
-const ImagemResponsive = styled.img`
-  width: 100%;
-  height: 120px;
-
-  @media (min-width: 640px) {
-    width: 200px;
-    height: 330px;
-  }
-
-  @media (min-width: 768px) {
-    width: 350px;
-    height: 365px;
-  }
-
-  @media (min-width: 1024px) {
-    width: 550px;
-    height: 365px;
-  }
-
-`;
-
-
 export function ResponsiveImage({ src, alt }) {
   return (
     <div className="image-container">
